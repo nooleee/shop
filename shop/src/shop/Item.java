deeabc346b2dc1b5478ce771016daa925fc85542
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-21 18:14:42 +0900

    create Item : getter, 기본 생성자, clone

--- shop/src/shop/Item.java
+++ shop/src/shop/Item.java
@@ -1,6 +1,27 @@
 package shop;
 
 public class Item {
+	private String name;
+	private int code;
+	
+	public Item() {
+		
+	}
+	
+	public Item(String name, int code) {
+		this.name = name;
+		this.code = code;
+	}
 
-
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+	public Item clone() {
+		return new Item(this.name, this.code);
+	}
 }
