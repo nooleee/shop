34e7aeac9a607296a6eec518594369f1a1ea8211
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-21 18:16:37 +0900

    create UserManager : 유저등록, 코드 중복 검사, 코드 생성, id중복검사, 유저삭제, 유저정보

--- shop/src/shop/UserManager.java
+++ shop/src/shop/UserManager.java
@@ -1,6 +1,82 @@
 package shop;
 
+import java.util.Random;
+import java.util.Vector;
+
 public class UserManager {
-
-
+	private Random ran = new Random();
+	private Vector<User> list;
+	
+	public UserManager() {
+		list.add(new User("admin","1234", 123));
+	}
+	
+	// 유저 등록
+	public User createUser(String id, String password) {
+		int code = generateUserCode();
+		if(!findUserByUserId(id)) {
+			User user = new User(id, password, code);
+			list.add(user);
+			return user.clone();
+		}
+		return new User();
+	}
+	
+	// 코드 중복 검사
+	public User findUserByUserCode(int code) {
+		for(User user : list) {
+			if(user.getCode() == code)
+				return user.clone();
+		}
+		return new User();
+	}
+	
+	// 코드 생성
+	private int generateUserCode() {
+		int code = 0;
+		while(true) {
+			code = ran.nextInt(9000) + 1000;
+			
+			User user = findUserByUserCode(code);
+			if(user.getCode() == 0)
+				break;
+		}
+		return code;
+	}
+	
+	// id중복 검사
+	public boolean findUserByUserId(String id) {
+		for(User user : list) {
+			if(user.getId().equals(id))
+				return true;
+		}
+		return false;
+	}
+	
+	// 유저 삭제
+	public boolean deleteUser(User user) {
+		int userCode = user.getCode();
+		User target = getUserByUserCode(userCode);
+		return list.remove(target);
+	}
+	
+	public int getUserSize() {
+		return this.list.size();
+	}
+	
+	// 유저 정보
+	public User getUserByUserCode(int code) {
+		for(User user : list) {
+			if(user.getCode() == code)
+				return user;
+		}
+		return new User();
+	}
+	
+	// 장바구니 담기
+	
+	// 장바구니 삭제
+	
+	// 장바구니 수정
+	
 }
