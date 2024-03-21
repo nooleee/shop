93a25d110d2ff4aa6ccfcbe7e1bddb6ce1157f2b
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-21 18:28:21 +0900

    create Shop : 기본 생성자, 현재 상태 표기, 메뉴 출력

--- shop/src/shop/Shop.java
+++ shop/src/shop/Shop.java
@@ -1,6 +1,26 @@
 package shop;
 
 public class Shop {
+	public Shop(String name) {
+		this.isRun = true;
+	}
+	private UserManager UserManager = new UserManager();
+	private ItemManager ItemManager = new ItemManager();
+	private boolean isRun;
+	
+	private void printStatus() {
+		int UserSize = UserManager.getUserSize();
+		int ItemSize = ItemManager.getItemSize();
+		String status = String.format("User2 size : %d\nAccount2 size : %d", UserSize, ItemSize);
 
-
+		System.out.println("------------");
+		System.out.println(status);
+	}
+	
+	public void run() {
+		while(isRun) {
+			printStatus();
+			break;
+		}
+	}
 }
