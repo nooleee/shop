a218a93089071fd92132967256fceb6a5ef3aa1d
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-21 18:13:30 +0900

    create User : getter, 기본 생성자, clone

--- shop/src/shop/User.java
+++ shop/src/shop/User.java
@@ -1,6 +1,40 @@
 package shop;
 
 public class User {
-	private String id, password, name;
+	private String id, password;
 	private int code;
+	private Cart cart;
+	
+	public User() {
+	}
+	
+	public User(String id, String password, int code) {
+		this.id = id;
+		this.password = password;
+		this.code = code;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+	public Cart getCart() {
+		return cart;
+	}
+	
+//	public void getCode(int code) {
+//		this.code = code;
+//	}
+	
+	public User clone() {
+		return new User(this.id, this.password, this.code);
+	}
 }
